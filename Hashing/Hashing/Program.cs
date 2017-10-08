d51d65f9181e078793af6a760e63fcb80257d4f1
sksmssk4 <tlvmak7@naver.com>
2017-10-09 04:49:48 +0900

    Searching Method
    search!

---
+++ Hashing/Hashing/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing
+{
+    public static class ExtensionMethods
+    {
+        static int[] Hash = { 1122, 22342, 5233415, 65677654 };
+        public static int GetMyHash(this String hash)
+        {
+            int hashcode = 0;
+            for (int i = 0; i < hash.Length; i++)
+                hashcode += Hash[i] * (hash[i] - 'a');
+            return hashcode;
+        }
+        static void Main(string[] args)
+        {
+            string hello = "hello";
+            int hashCode = hello.GetMyHash();
+            Console.WriteLine(hashCode);
+        }
+    }
+}
--- Hashing/Program.cs
+++ Hashing/Program.cs
@@ -6,11 +6,24 @@ using System.Threading.Tasks;
 
 namespace Hashing
 {
-    class Program
+    public static class HashMethod
     {
+        static string[] Hash = { "ab","ac","ad","ae","af","ag"}; // n개의 array형태 버킷
+        
+        public static string HashTable(this string hash)
+        {
+            string solution = "";
+            solution = Hash[0];
+            return solution;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("hello");
+            string s= "";
+            string hashValue = s.HashTable();
+            Console.Write("값 입력 : ");
+            s = Console.ReadLine();
+            Console.WriteLine($"{hashValue}", hashValue);
         }
     }
+
 }
